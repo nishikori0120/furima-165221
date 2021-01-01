39c3da9c361f5664024cc712e9a3ac3e3f8cf92d
nishikori0120 <yu.nishikori@di-v.co.jp>
2021-01-01 19:18:12 +0900

    カテゴリーの選択機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,11 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:itemname,:iteminfo,:price,:category_id,:image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :user
+  has_one_attached :image
+
+  #空の投稿を保存できないようにする
+  validates :itemname, :iteminfo, :price, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 } 
 end
