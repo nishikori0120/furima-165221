3f556a121760a32b571f1bd8b6a7ee0c39df3097
nishikori0120 <yu.nishikori@di-v.co.jp>
2021-01-01 20:58:37 +0900

    出品登録時のエラーメッセージ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,14 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    item = Item.new(item_params)
+    if item.save
+      redirect_to root_path
+    else
+      @item = Item.new(item_params)
+      @item.valid?
+      render 'new'
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,10 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   #空の投稿を保存できないようにする
-  validates :itemname, :iteminfo, :price, presence: true
+  validates :itemname, presence: true
+  validates :iteminfo, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1 } 
